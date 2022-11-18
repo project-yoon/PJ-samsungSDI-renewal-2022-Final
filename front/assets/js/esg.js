3e99fb2bd4abe401cf4bdd2935788bdfb0a0155c
project-yoon <47205814+project-yoon@users.noreply.github.com>
2022-11-18 17:33:05 +0900

    [pub] 6주차(11.07~11.10) 진행사항 : ESG카테고리 완료

--- front/assets/js/esg.js
+++ front/assets/js/esg.js
@@ -1,3 +1,12 @@
 
+function popQuickOn() {
+    $('.btn_top').on('click', function(){
+        var idx = $(this).parent().parent().children('.pop_body').children('.pop_inner')
+        $(idx).animate({scrollTop: 0}, 400)
+    })
+}
+
 $(document).ready(function() {
+    popQuickOn();
+
 })
