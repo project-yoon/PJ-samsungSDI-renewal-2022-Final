bce3b492e8c35a1168ad50db33c2263d4420fd64
yyxxmm-codepar <yyxxmm@codepartners.co.kr>
2023-02-15 11:11:34 +0900

    [pub] [common] 공통 사항 업데이트

--- guide/assets/js/sdi_now.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function(){
-    if(window.innerWidth <= 1280) {
-        $('.move_area').parent().removeClass('content_inner')
-    } else {
-        $('.move_area').parent().addClass('content_inner')
-    }
-})
